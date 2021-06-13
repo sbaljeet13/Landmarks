e04b654ecb392a8887b5be6e6e1c6d3dcb6842df
Baljeet Singh <22825252+sbaljeet13@users.noreply.github.com>
2021-06-12 23:47:39 -0700

    Add Landkmarks List in  a Navigation View

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,27 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Baljeet Singh on 2021-06-11.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink(destination: LandmarkDetail()) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+        }
+        .navigationTitle("Landmarks")
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
